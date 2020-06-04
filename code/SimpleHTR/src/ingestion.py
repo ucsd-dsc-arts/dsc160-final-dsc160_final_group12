31fb1187e311ae1d9388f823034b507ad0846c49
mhrowlan <43191865+mhrowlan@users.noreply.github.com>
2020-06-04 10:26:45 -0700

    y is this so hard

---
+++ code/SimpleHTR/src/ingestion.py
@@ -0,0 +1,71 @@
+""" Image Ingestion
+
+ingestion.py allows users to generate and upload an 
+image of their choice
+
+"""
+
+# Importing scripts
+import pandas as pd
+import numpy as np
+import panel as pn
+pn.extension()
+
+import holoviews as hv
+from holoviews import opts
+from holoviews import streams
+hv.extension('bokeh')
+
+
+
+def draw():
+    """
+    Allows users to draw and download an image
+    of their choice
+    
+    :returns: Widget to draw
+    """
+    
+    # Creating widget
+    path = hv.Path([])
+    freehand = streams.FreehandDraw(source=path, num_objects=100,
+                                    styles={'line_color': ['black']})
+    
+    # Customizing
+    path.opts(
+    opts.Path(active_tools=['freehand_draw'], height=219, 
+              line_width=8, width=519, xaxis=None, yaxis=None))
+    
+    return path
+
+    
+
+filepath = ''
+def input_data():
+    """
+    Allows users to upload an image of their 
+    choice
+    
+    :returns: panel widget to input file
+    """
+    
+    file_input = pn.widgets.FileInput()
+    
+    @pn.depends(file_input.param.filename)
+    def check_input(file):
+        """
+        Checks if input file has been passed in.
+        If so, saves data into directory
+        """
+        
+        if pd.isnull(file_input.filename):
+            return file_input
+        
+        else:
+            global filepath
+            filepath = file_input.filename
+            file_input.save(filepath)
+        
+        return file_input
+    
+    return pn.Row(check_input)
