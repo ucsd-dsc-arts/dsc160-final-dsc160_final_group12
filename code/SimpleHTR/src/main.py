32f8ab322da5067bde1b8ce7e3c6de7d8170b1cf
mhrowlan <43191865+mhrowlan@users.noreply.github.com>
2020-06-05 18:43:03 -0700

    ?

---
+++ code/SimpleHTR/src/main.py
@@ -0,0 +1,286 @@
+from __future__ import division
+from __future__ import print_function
+
+import sys
+import argparse
+import cv2
+import editdistance
+from DataLoader import DataLoader, Batch
+from Model import Model, DecoderType
+from SamplePreprocessor import preprocess
+import logging
+import tensorflow as tf
+tf.get_logger().setLevel(logging.ERROR)
+from turtle import *
+import random
+import pandas as pd
+
+
+
+class FilePaths:
+    "filenames and paths to data"
+    fnCharList = '../model/charList.txt'
+    fnAccuracy = '../model/accuracy.txt'
+    fnTrain = '../data/'
+    fnInfer = '../data/text_image.png'
+    fnCorpus = '../data/corpus.txt'
+
+
+
+def train(model, loader):
+    "train NN"
+    epoch = 0 # number of training epochs since start
+    bestCharErrorRate = float('inf') # best valdiation character error rate
+    noImprovementSince = 0 # number of epochs no improvement of character error rate occured
+    earlyStopping = 5 # stop training after this number of epochs without improvement
+    while True:
+        epoch += 1
+        print('Epoch:', epoch)
+
+        # train
+        print('Train NN')
+        loader.trainSet()
+        while loader.hasNext():
+            iterInfo = loader.getIteratorInfo()
+            batch = loader.getNext()
+            loss = model.trainBatch(batch)
+            print('Batch:', iterInfo[0],'/', iterInfo[1], 'Loss:', loss)
+
+        # validate
+        charErrorRate = validate(model, loader)
+
+        # if best validation accuracy so far, save model parameters
+        if charErrorRate < bestCharErrorRate:
+            print('Character error rate improved, save model')
+            bestCharErrorRate = charErrorRate
+            noImprovementSince = 0
+            model.save()
+            open(FilePaths.fnAccuracy, 'w').write('Validation character error rate of saved model: %f%%' % (charErrorRate*100.0))
+        else:
+            print('Character error rate not improved')
+            noImprovementSince += 1
+
+        # stop training if no more improvement in the last x epochs
+        if noImprovementSince >= earlyStopping:
+            print('No more improvement since %d epochs. Training stopped.' % earlyStopping)
+            break
+
+word=''
+probablity=0
+
+def validate(model, loader):
+    "validate NN"
+    print('Validate NN')
+    loader.validationSet()
+    numCharErr = 0
+    numCharTotal = 0
+    numWordOK = 0
+    numWordTotal = 0
+    while loader.hasNext():
+        iterInfo = loader.getIteratorInfo()
+        print('Batch:', iterInfo[0],'/', iterInfo[1])
+        batch = loader.getNext()
+        (recognized, _) = model.inferBatch(batch)
+        
+        print('Ground truth -> Recognized')	
+        for i in range(len(recognized)):
+            numWordOK += 1 if batch.gtTexts[i] == recognized[i] else 0
+            numWordTotal += 1
+            dist = editdistance.eval(recognized[i], batch.gtTexts[i])
+            numCharErr += dist
+            numCharTotal += len(batch.gtTexts[i])
+            print('[OK]' if dist==0 else '[ERR:%d]' % dist,'"' + batch.gtTexts[i] + '"', '->', '"' + recognized[i] + '"')
+    
+    # print validation result
+    charErrorRate = numCharErr / numCharTotal
+    wordAccuracy = numWordOK / numWordTotal
+    #print('Character error rate: %f%%. Word accuracy: %f%%.' % (charErrorRate*100.0, wordAccuracy*100.0))
+    return charErrorRate
+
+
+def infer(model, fnImg):
+    "recognize text in image provided by file path"
+    img = preprocess(cv2.imread(fnImg, cv2.IMREAD_GRAYSCALE), Model.imgSize)
+    batch = Batch(None, [img])
+    model_infer = model.inferBatch(batch, True)
+    if len(model_infer) == 3:
+        (recognized, probability, _) = model_infer
+    else:
+        (recognized, probability) = model_infer
+    print('Recognized:', '"' + recognized[0] + '"')
+    print('Probability:', probability[0])
+<<<<<<< HEAD
+    return (recognized[0],probability[0])
+=======
+    
+    return recognized, probability
+>>>>>>> dd4c864796fc22fde4e234ed174af7acbc414cb4
+
+
+def main():
+    "main function"
+    # optional command line args
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train', help='train the NN', action='store_true')
+    parser.add_argument('--validate', help='validate the NN', action='store_true')
+    parser.add_argument('--beamsearch', help='use beam search instead of best path decoding', action='store_true')
+    parser.add_argument('--wordbeamsearch', help='use word beam search instead of best path decoding', action='store_true')
+    parser.add_argument('--dump', help='dump output of NN to CSV file(s)', action='store_true')
+
+    args = parser.parse_args()
+
+    decoderType = DecoderType.BestPath
+    if args.beamsearch:
+        decoderType = DecoderType.BeamSearch
+    elif args.wordbeamsearch:
+        decoderType = DecoderType.WordBeamSearch
+
+    # train or validate on IAM dataset	
+    if args.train or args.validate:
+        # load training data, create TF model
+        loader = DataLoader(FilePaths.fnTrain, Model.batchSize, Model.imgSize, Model.maxTextLen)
+
+        # save characters of model for inference mode
+        open(FilePaths.fnCharList, 'w').write(str().join(loader.charList))
+        
+        # save words contained in dataset into file
+        open(FilePaths.fnCorpus, 'w').write(str(' ').join(loader.trainWords + loader.validationWords))
+
+        # execute training or validation
+        if args.train:
+
+            model = Model(loader.charList, decoderType)
+            train(model, loader)
+        elif args.validate:
+            model = Model(loader.charList, decoderType, mustRestore=True)
+            validate(model, loader)
+
+    # infer text on test image
+    else:
+        #print(open(FilePaths.fnAccuracy).read())
+        tf.compat.v1.reset_default_graph()
+        model = Model(open(FilePaths.fnCharList).read(), decoderType, mustRestore=True, dump=args.dump)
+<<<<<<< HEAD
+        
+        word, probablity=infer(model, FilePaths.fnInfer)
+        
+        letter_color_map = {'a': 'aqua', 'b': 'azure', 'c': 'beige', 
+                    'd': 'black','e': 'blue', 'f': 'brown', 
+                    'g': 'chartreuse', 'h': 'chocolate', 'i': 'coral', 
+                    'j': 'crimson', 'k': 'cyan', 'l': 'darkblue', 
+                    'm': 'darkgreen', 'n': 'fuchsia', 'o': 'gold', 
+                    'p': 'goldenrod', 'q': 'green', 'r': 'grey', 
+                    's': 'indigo', 't': 'ivory', 'u': 'lavender', 
+                    'v': 'lightblue', 'w': 'lightgreen', 'x': 'maroon', 
+                    'y': 'olive', 'z': 'orange'}
+        word= word.lower()
+        letters= list(word)
+        colors=list((pd.Series(letters)).map(letter_color_map))
+        print(colors)
+        number= random.choice([0,1,2])
+        if number==1:
+            #draw hexogram-sprial
+            setup()
+            title("hexogram sprial")
+            t1 = Turtle()
+            bgcolor('black')
+
+            #hide the turtle icon
+            t1.hideturtle()
+            t1.speed(0)
+            for i in range (200):
+                #choose a random color for the turtle
+                colorchoice = random.choice(colors)
+                #have the turtle take on the randomly chose color
+                t1.color(colorchoice)
+                t1.forward(i)
+                t1.left(59)
+            #t1.done()
+            try:
+                t1.exitonclick()   
+            except Excpetion:
+                pass
+        elif number==2:
+            #draw color spirograph
+            setup()
+            title("spirograph")
+            #create a turtle for drawing
+            t1 = Turtle()
+            bgcolor('black')
+            #change the pen thickness
+            t1.width(2)
+            #hide the turtle icon
+            t1.hideturtle()
+            #set turtle speed to MAXIMUM (1-10 for specific speeds. 1 is slowest)
+            t1.speed(0)
+
+            #create a loop for the graphics to be built
+            for i in range(50):
+                #choose a random color for the turtle
+                colorchoice = random.choice(colors)
+                #have the turtle take on the randomly chose color
+                t1.color(colorchoice)
+                #create circle
+                t1.circle(100)
+                t1.left(10)
+
+            #t1.done()
+            try:
+                t1.exitonclick()   
+            except Excpetion:
+                pass
+        else:
+            #color wheel
+            setup()
+            title("colorwheel")
+            t1 = Turtle()
+                        #do some basic setup for the turtle
+            #pick up the pen so no marks are left
+            t1.up()
+            #move the turtle to the left
+            t1.goto(-200,0)
+            #put the pen back down
+            t1.down()
+            #change the pen thickness
+            t1.width(2)
+            #hide the turtle icon
+            t1.hideturtle()
+            #set turtle speed to MAXIMUM (1-10 for specific speeds. 1 is slowest)
+            t1.speed(0)
+
+            #create a loop for the graphics to be built
+            for i in range(500):
+                #choose a random color for the turtle
+                colorchoice = random.choice(colors)
+                #have the turtle take on the randomly chose color
+                t1.color(colorchoice)
+                #move the turtle forward
+                t1.forward(400)
+                #have the turtle turn 181 degrees (anything over 180 works)
+                t1.right(181)
+
+            #t1.done() 
+            try:
+                t1.exitonclick()   
+            except Excpetion:
+                pass
+
+           
+            
+
+        
+    
+    
+=======
+        infer(model, FilePaths.fnInfer)
+        print('done')
+        return infer
+>>>>>>> dd4c864796fc22fde4e234ed174af7acbc414cb4
+
+
+if __name__ == '__main__':
+    main()
+    
+    
+
